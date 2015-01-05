588026af37f57dc780afcf521b74c54ff93327da
William Tang <wtang@squorange.com>
2015-01-04 16:38:15 -0800

    Footer implemented

--- wp-content/themes/ag-theme/parts/top-bar.php
+++ wp-content/themes/ag-theme/parts/top-bar.php
@@ -1,13 +1,28 @@
-<div class="top-bar-container contain-to-grid show-for-medium-up">
-    <nav class="top-bar" data-topbar role="navigation">
-        <ul class="title-area">
-            <li class="name">
-                <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
-            </li>
-        </ul>
-        <section class="top-bar-section">
-            <?php foundationPress_top_bar_l(); ?>
-            <?php foundationPress_top_bar_r(); ?>
-        </section>
-    </nav>
-</div>
+<!-- NAV BAR -->
+
+	<div class="fixed">
+		<nav class="top-bar l-nav" data-topbar>
+		  <ul class="title-area">
+		    <li class="name">
+		      <a class="nav__logo" href="<?php echo home_url(); ?>/"></a>
+		    </li>
+		    <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+		  </ul>
+		  <section class="top-bar-section">		    
+				<!-- Left Nav Section -->
+		    <ul class="left nav__left">
+		      <li class="menu__item"><a href="<?php echo home_url(); ?>/share/">Share</a></li>
+		      <li class="menu__item"><a href="<?php echo home_url(); ?>/build/">Build</a></li>
+		      <li class="menu__item"><a href="<?php echo home_url(); ?>/advise/">Advise</a></li>
+		      <li class="menu__item"><a href="<?php echo home_url(); ?>/give/">Give</a></li>					
+		    </ul>
+		    <!-- Right Nav Section -->
+		    <ul class="right nav__right">
+		      <li class="menu__item"><a href="<?php echo home_url(); ?>/projects/">Projects</a></li>
+		      <li class="menu__item"><a href="<?php echo home_url(); ?>/team/">Team</a></li>
+		      <li class="menu__item"><a href="<?php echo home_url(); ?>/about/">About</a></li>
+		      <li class="menu__item"><a href="<?php echo home_url(); ?>/contact/">Contact</a></li>					
+		    </ul>
+		  </section>
+		</nav>
+	</div>
